c4860457952a7bb30693e5e8f35ff152edc8f196
greyphoenix1 <rahmanmukit905@gmail.com>
2025-04-13 15:00:37 -0400

    Added Horizontal Scroll and Content

--- portfo/porto.js
+++ portfo/porto.js
@@ -5,4 +5,13 @@ document.addEventListener("DOMContentLoaded", function() {
       document.querySelector('.cover-t').classList.toggle('open');
       document.querySelector('.cover-b').classList.toggle('open');
     });
+
+
+    const content = document.querySelector('.content');
+
+    content.addEventListener('wheel', function(e) {
+      e.preventDefault(); // no more vertical scroll
+
+      content.scrollLeft -= e.deltaY; // horizontal scroll
+    });
 });
